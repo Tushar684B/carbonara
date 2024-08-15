860e3f5fdf5221269ff1d2717eddc0c8aaa921ba
Tushar Bharadwaj <ttushar684@gmail.com>
2024-08-15 12:01:46 +0530

    remove shapefile

--- carbonarr/common.py
+++ carbonarr/common.py
@@ -6,26 +6,6 @@ def hello_world():
     """
     print("Hello World!")
 
-import geopandas as gpd
-import matplotlib.pyplot as plt
 
-def shapefile(shapefile_path):
-    # Load the shapefile
-    gdf = gpd.read_file(shapefile_path)
-    
-    # Plot the shapefile and zoom to its extent
-    ax = gdf.plot(figsize=(10, 10), edgecolor='k')
-    
-    # Set the x and y limits to the bounds of the shapefile
-    xmin, ymin, xmax, ymax = gdf.total_bounds
-    ax.set_xlim(xmin, xmax)
-    ax.set_ylim(ymin, ymax)
-    
-    # Add title and display plot
-    plt.title("Zoomed Shapefile")
-    plt.xlabel("Longitude")
-    plt.ylabel("Latitude")
-    plt.show()
 
-# Example usage:
-# shapefile("path_to_your_shapefile.shp")
+
