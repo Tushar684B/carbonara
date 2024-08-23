3212a23bae7fbcdbebf9bf24bab8ddfc836a7278
Tushar Bharadwaj <ttushar684@gmail.com>
2024-08-23 15:06:16 +0530

    hello world name

--- carbonarr/common.py
+++ carbonarr/common.py
@@ -1,10 +1,11 @@
 """The common module contains common functions and classes used by the other modules.
 """
 
-def hello_world():
+def hello_world(name):
     """Prints "Hello World!" to the console.
     """
-    print("Hello World!")
+    name = input()
+    print("Hello World!"+" "+name)
 
 
 
