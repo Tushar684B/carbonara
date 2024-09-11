e68b9edee32218d30b89b3f7bbf32e88cb996d3e
Tushar Bharadwaj <ttushar684@gmail.com>
2024-09-11 10:36:49 +0530

    commit

--- carbonarr/carbonarr.py
+++ carbonarr/carbonarr.py
@@ -2,7 +2,7 @@
 import json
 import ipyleaflet
 import geopandas as gpd
-from ipyleaflet import basemaps, GeoJSON
+from ipyleaflet import basemaps, GeoJSON, LayersControl
 
 
 class Map(ipyleaflet.Map):
@@ -55,9 +55,10 @@ class Map(ipyleaflet.Map):
         Args:
             position (str, optional): Position of the layers control on the map. Defaults to 'topright'.
         """
-        self.add(ipyleaflet.LayersControl(position=position))
+        self.add_control(LayersControl(position=position))
 
-    def add_geojson_layer(self, filepath, name, **kwargs):
+    def add_geojson_layer(self, filepath, name='geojson', **kwargs):
+
         """Adds a GeoJSON layer to the map from a file.
 
         Args:
@@ -65,6 +66,9 @@ class Map(ipyleaflet.Map):
             name (str): The name of the GeoJSON layer.
             **kwargs: Additional keyword arguments for the GeoJSON layer.
         """
+
+        # import json
+        # if isinstance(data, str):
         with open(filepath, 'r') as f:
             data = json.load(f)
         
@@ -87,32 +91,3 @@ class Map(ipyleaflet.Map):
         # Create a GeoJSON layer and add it to the map
         geo_json = GeoJSON(data=data, **kwargs)
         self.add(geo_json)
-class ARR:
-    """A class to extract and display Esri LULC maps for 2021 and 2023.
-
-    This class uses Esri LULC map data to visualize land use and land cover changes over time on a map.
-    """
-
-    def __init__(self, map_instance):
-        """Initializes the ARR class with a given map instance.
-
-        Args:
-            map_instance (Map): An instance of the Map class where the layers will be displayed.
-        """
-        self.map = map_instance
-
-    def add_lulc_layer(self, year):
-        """Adds an Esri LULC map layer for a specific year to the map.
-
-        Args:
-            year (int): The year of the LULC map (e.g., 2021 or 2023).
-        """
-        url_template = f"https://services.arcgisonline.com/arcgis/rest/services/ESRI_LandCover_{year}/MapServer/tile/{{z}}/{{y}}/{{x}}"
-        name = f"Esri LULC {year}"
-        self.map.add_tile_layer(url=url_template, name=name)
-
-    def show_lulc_maps(self):
-        """Adds Esri LULC map layers for 2021 and 2023 to the map."""
-        self.add_lulc_layer(2021)
-        self.add_lulc_layer(2023)
-        self.map.add_layers_control()
